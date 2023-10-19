28023b8fca932f60014b263936533984e8ada872
maxkinho <antoniomsgfilho@gmail.com>
2023-10-19 15:25:26 -0400

    Implementação da camada de domínio.

---
+++ src/main/java/com/dio/Application.java
@@ -0,0 +1,44 @@
+package com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+
+//Java RESTful API criada para a conclusão do Bootcamp Santander - Backend (Java) em parceria com a DIO.
+
+//santander-dev-week-2023
+
+//{
+//  "name": "Antonio",
+//  "account": {
+//    "number": "00000000-0",
+//    "agency": "0000",
+//    "balance": 1234.64,
+//    "limit": 1000.00
+//  },
+//  "features": [
+//    {
+//      "icon": "URL",
+//      "description": "Descrição da feature"
+//    }
+//  ],
+//  "card": {
+//    "number": "xxxx xxxx xxxx xxxx",
+//    "limit": 1000.00
+//  },
+//  "news": [
+//    {
+//      "icon": "URL",
+//      "description": "Descrição da novidade"
+//    }
+//  ]
+//}
